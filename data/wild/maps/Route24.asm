6cd2fd76cbd1a6668f0fce9bf19ff01a927a1d54
Travis <opcodestop@gmail.com>
2022-06-05 16:21:27 -0500

    Route 24 encounters

--- data/wild/maps/Route24.asm
+++ data/wild/maps/Route24.asm
@@ -1,26 +1,15 @@
 Route24WildMons:
 	def_grass_wildmons 25 ; encounter rate
-IF DEF(_RED)
-	db  7, WEEDLE
-	db  8, KAKUNA
-	db 12, PIDGEY
-	db 12, ODDISH
-	db 13, ODDISH
-	db 10, ABRA
-	db 14, ODDISH
-ENDC
-IF DEF(_BLUE)
-	db  7, CATERPIE
-	db  8, METAPOD
-	db 12, PIDGEY
-	db 12, BELLSPROUT
-	db 13, BELLSPROUT
-	db 10, ABRA
-	db 14, BELLSPROUT
-ENDC
-	db 13, PIDGEY
-	db  8, ABRA
-	db 12, ABRA
+	db  12, PIDGEY     ; 19.9%
+	db  12, SPEAROW    ; 19.9%
+	db  12, PSYDUCK    ; 15.2%
+	db  13, PSYDUCK    ; 9.8%
+	db  12, BELLSPROUT ; 9.8%
+	db  13, BELLSPROUT ; 9.8%
+	db  10, ABRA       ; 5.1%
+	db  11, ABRA       ; 5.1%
+	db  13, BUTTERFREE ; 4.3%
+	db  14, BEEDRILL   ; 1.2%
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
