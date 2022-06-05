022b1b8a3f7623a3b74eec39073dbe5582e9cf8e
Travis <opcodestop@gmail.com>
2022-06-05 16:08:02 -0500

    Route 4 encounters

--- data/wild/maps/Route4.asm
+++ data/wild/maps/Route4.asm
@@ -1,26 +1,15 @@
 Route4WildMons:
 	def_grass_wildmons 20 ; encounter rate
-	db 10, RATTATA
-	db 10, SPEAROW
-	db  8, RATTATA
-IF DEF(_RED)
-	db  6, EKANS
-	db  8, SPEAROW
-	db 10, EKANS
-	db 12, RATTATA
-	db 12, SPEAROW
-	db  8, EKANS
-	db 12, EKANS
-ENDC
-IF DEF(_BLUE)
-	db  6, SANDSHREW
-	db  8, SPEAROW
-	db 10, SANDSHREW
-	db 12, RATTATA
-	db 12, SPEAROW
-	db  8, SANDSHREW
-	db 12, SANDSHREW
-ENDC
+	db  10, SANDSHREW  ; 19.9%
+	db  10, EKANS      ; 19.9%
+	db  12, BELLSPROUT ; 15.2%
+	db  9, PSYDUCK     ; 9.8%
+	db  10, PSYDUCK    ; 9.8%
+	db  10, BELLSPROUT ; 9.8%
+	db  12, SANDSHREW  ; 5.1%
+	db  12, EKANS      ; 5.1%
+	db  12, PSYDUCK    ; 4.3%
+	db  10, ABRA       ; 1.2%
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
