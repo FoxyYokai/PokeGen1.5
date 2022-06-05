0bba3c543b0bff467b8c869878d3d6b4804c2886
Travis <opcodestop@gmail.com>
2022-06-05 01:09:03 -0500

    Route 1 and 2 encounter changes

--- data/wild/maps/Route1.asm
+++ data/wild/maps/Route1.asm
@@ -1,15 +1,15 @@
 Route1WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  3, PIDGEY
-	db  3, RATTATA
-	db  3, RATTATA
-	db  2, RATTATA
-	db  2, PIDGEY
-	db  3, PIDGEY
-	db  3, PIDGEY
-	db  4, RATTATA
-	db  4, PIDGEY
-	db  5, PIDGEY
+	db  3, PIDGEY    ; 19.9%
+	db  3, RATTATA   ; 19.9%
+	db  2, CATERPIE  ; 15.2%
+	db  2, RATTATA   ; 9.8%
+	db  2, PIDGEY    ; 9.8%
+	db  3, CATERPIE  ; 9.8%
+	db  3, NIDORAN_M ; 5.1%
+	db  3, NIDORAN_F ; 5.1%
+	db  4, PIDGEY    ; 4.3%
+	db  4, RATTATA   ; 1.2%
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
