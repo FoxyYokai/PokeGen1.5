48553b7a6ff1abd4aef5ecc8f70e6205bb2bb152
Travis <opcodestop@gmail.com>
2022-06-08 00:00:21 -0500

    Rock Tunnel encounters

--- data/wild/maps/RockTunnelB1F.asm
+++ data/wild/maps/RockTunnelB1F.asm
@@ -1,15 +1,15 @@
 RockTunnelB1FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 16, ZUBAT
-	db 17, ZUBAT
-	db 17, GEODUDE
-	db 15, MACHOP
-	db 16, GEODUDE
-	db 18, ZUBAT
-	db 17, MACHOP
-	db 17, ONIX
-	db 13, ONIX
-	db 18, GEODUDE
+	db  20, GEODUDE    ; 19.9%
+	db  20, ZUBAT      ; 19.9%
+	db  21, GEODUDE    ; 15.2%
+	db  21, ZUBAT      ; 9.8%
+	db  18, MACHOP     ; 9.8%
+	db  19, MACHOP	   ; 9.8%
+	db  20, MACHOP     ; 5.1%
+	db  22, ONIX	   ; 5.1%
+	db  24, ONIX	   ; 4.3%
+	db  20, MR_MIME    ; 1.2%
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
