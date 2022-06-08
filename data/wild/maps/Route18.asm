0ea5392cbc919489aa382797cdbe8644eab8c4cd
Travis <opcodestop@gmail.com>
2022-06-08 11:05:45 -0500

    Routes 16-18 encounters

--- data/wild/maps/Route18.asm
+++ data/wild/maps/Route18.asm
@@ -1,15 +1,15 @@
 Route18WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 20, SPEAROW
-	db 22, SPEAROW
-	db 25, RATICATE
-	db 24, DODUO
-	db 25, FEAROW
-	db 26, DODUO
-	db 28, DODUO
-	db 29, RATICATE
-	db 27, FEAROW
-	db 29, FEAROW
+	db  27, SANDSLASH  ; 19.9%
+	db  27, ARBOK      ; 19.9%
+	db  28, FEAROW     ; 15.2%
+	db  28, SANDSLASH  ; 9.8%
+	db  28, ARBOK      ; 9.8%
+	db  29, FEAROW     ; 9.8%
+	db  29, NIDORINO   ; 5.1%
+	db  29, NIDORINA   ; 5.1%
+	db  30, DITTO	   ; 4.3%
+	db  31, DITTO      ; 1.2%
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
