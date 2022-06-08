668f7e3019f8c2ee6673a797cee509439e4d815a
Travis <opcodestop@gmail.com>
2022-06-08 15:09:25 -0500

    open sea routes

--- data/wild/maps/Route21.asm
+++ data/wild/maps/Route21.asm
@@ -13,16 +13,16 @@ Route21WildMons:
 	end_grass_wildmons
 
 	def_water_wildmons 5 ; encounter rate
-	db  5, TENTACOOL
-	db 10, TENTACOOL
-	db 15, TENTACOOL
-	db  5, TENTACOOL
-	db 10, TENTACOOL
-	db 15, TENTACOOL
-	db 20, TENTACOOL
-	db 30, TENTACOOL
-	db 35, TENTACOOL
-	db 40, TENTACOOL
+	db  27, TENTACOOL   ; 19.9%
+	db  29, TENTACOOL   ; 19.9%
+	db  28, HORSEA		; 15.2%
+	db  29, HORSEA      ; 9.8%
+	db  27, STARYU      ; 9.8%
+	db  28, STARYU      ; 9.8%
+	db  33, TENTACRUEL  ; 5.1%
+	db  33, SEADRA      ; 5.1%
+	db  35, GYARADOS    ; 4.3%
+	db  33, LAPRAS      ; 1.2%
 	end_water_wildmons
 
 	def_fishing_wildmons 1 ; encounter rate
