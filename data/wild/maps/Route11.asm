2688d077418dfa4f26abca50d2fda3d3621d94e8
Travis <opcodestop@gmail.com>
2022-06-07 23:29:05 -0500

    Route11 and Diglett's Cave changes

--- data/wild/maps/Route11.asm
+++ data/wild/maps/Route11.asm
@@ -1,26 +1,15 @@
 Route11WildMons:
-	def_grass_wildmons 15 ; encounter rate
-IF DEF(_RED)
-	db 14, EKANS
-	db 15, SPEAROW
-	db 12, EKANS
-	db  9, DROWZEE
-	db 13, SPEAROW
-	db 13, DROWZEE
-	db 15, EKANS
-ENDC
-IF DEF(_BLUE)
-	db 14, SANDSHREW
-	db 15, SPEAROW
-	db 12, SANDSHREW
-	db  9, DROWZEE
-	db 13, SPEAROW
-	db 13, DROWZEE
-	db 15, SANDSHREW
-ENDC
-	db 17, SPEAROW
-	db 11, DROWZEE
-	db 15, DROWZEE
+	def_grass_wildmons 20 ; encounter rate
+	db  17, SPEAROW    ; 19.9%
+	db  17, VULPIX     ; 19.9%
+	db  18, RATTATA    ; 15.2%
+	db  17, GROWLITHE  ; 9.8%
+	db  17, DROWZEE    ; 9.8%
+	db  18, DROWZEE	   ; 9.8%
+	db  19, NIDORINO   ; 5.1%
+	db  19, NIDORINA   ; 5.1%
+	db  19, LICKITUNG  ; 4.3%
+	db  20, LICKITUNG  ; 1.2%
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
